1bbce4e36e12f23eb788f796d2f285c0a015e4a2
unknown <auliyaihsani@gmail.com>
2019-09-01 16:34:40 +0700

    initial commit

---
+++ src/components/Addtodo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+      <h3>add Todo</h3>
+      <div class="add">
+        <form @submit="onSubmit">
+          <input type="text" v-model="title" placeholder="add Todo..">
+          <input type="submit" value="Submit" >
+        </form>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    name: "Addtodo",
+    data(){
+      return {
+        title: ''
+      }
+    }, 
+    methods :{
+       ...mapActions(['addTodo']),
+       onSubmit(e){
+         e.preventDefault();
+         this.addTodo(this.title);
+       }
+    }
+   
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 10px;
+  border: 1px solid #41b883;
+  outline: 0;
+}
+
+input[type="submit"]{
+  flex: 2;
+  background: #41b883;
+  color: #fff;
+  border: #41b883 solid;
+  cursor: pointer;
+}
+</style>>
+
---
+++ src/components/Todos.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+  <h3>haii Todos ini vue js</h3>
+  <div class="legend">
+    <span>double click to mark as complite</span>
+    <span>
+      <span class="incomplite-box"></span> = in Complite
+    </span>
+    <span>
+      <span class="complite-box"></span> = Complite
+    </span>
+  </div>
+
+
+  <div class="todos">
+    <div v-for="todo in allTodos" :key="todo.id" class="todo">{{ todo.title }}
+       <i @click="deleteTodo(todo.id)"  class="fa fa-trash" ></i>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+
+export default {
+    name: "Todos",
+    methods: {
+      ...mapActions(['fetchTodos', 'deleteTodo'])
+    },
+    computed: mapGetters(['allTodos']), 
+    created() {
+      this.fetchTodos();
+    }
+}
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.complite-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e; 
+
+}
+
+
+.incomplite-box{
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+
+}
+
+
+
+@media (max-width: 500px){
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
+
+</style>>
+
+
+ 
+
+
